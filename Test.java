0a7c25a473dee9116b6cdf863f87bfa92e7f8bd8
sindhuuu15 <vvce22cseaiml0089@vvce.ac.in>
2025-03-03 15:44:46 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,19 +1,14 @@
-
-// Java program to find area
-// of circle
-     
+import java.util.Scanner;
 class Test
 {
-    static final double PI = Math.PI;
-          
-    static double findArea(int r)
-    {
-       return PI * Math.pow(r, 2);
-    }
-          
-    // Driver method
-    public static void main(String[] args) 
-    {
-        System.out.println("Area is " +  findArea(5));
-    }
+   public static void main(String args[]) 
+    {   
+       
+      Scanner s= new Scanner(System.in);
+        
+         System.out.println("Enter the radius:");
+         double r= s.nextDouble();
+         double  area=(22*r*r)/7 ;
+         System.out.println("Area of Circle is: " + area);      
+   }
 }
